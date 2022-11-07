8a59c8c17c74f2794458dc700dfd886d7635df40
lucassadeck26 <lucaspiece21@gmail.com>
2022-11-07 17:29:07 -0500

    feat/test/games

--- src/data-source.ts
+++ src/data-source.ts
@@ -11,19 +11,6 @@ const AppDataSource = new DataSource(
       }
     : {
         type: 'postgres',
-        host: process.env.HOST,
-        port: 5432,
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-        logging: true,
-        synchronize: false,
-        entities: ['src/entities/*.ts'],
-        migrations: ['src/migrations/*.ts'],
-      } /*
-   {
-    
-        type: 'postgres',
         url: process.env.DATABASE_URL,
         ssl:
           process.env.NODE_ENV === 'production'
@@ -39,7 +26,7 @@ const AppDataSource = new DataSource(
           process.env.NODE_ENV === 'production'
             ? ['dist/src/migrations/*.js']
             : ['src/migrations/*.ts'],
-      }*/,
+      },
 );
 
 export default AppDataSource;
